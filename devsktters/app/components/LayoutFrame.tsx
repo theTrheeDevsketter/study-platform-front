4defdf5d622ef9b5b3fcbe030a8ef78edda3a77f
adriel <adrielarochaoronoz@gmail.com>
2023-02-14 17:30:58 +0000

    🦎 adding swr

--- devsktters/app/components/LayoutFrame.tsx
+++ devsktters/app/components/LayoutFrame.tsx
@@ -1,7 +1,10 @@
 'use client'
 
-import { useUser } from "@auth0/nextjs-auth0/client";
+import { UserProfile, useUser } from "@auth0/nextjs-auth0/client";
+import { UserProps } from "@auth0/nextjs-auth0/dist/client/with-page-auth-required";
 import Link from "next/link";
+import { useEffect } from "react";
+import useSWR from 'swr'
 
 import Navbar from "./HamburguerMenu";
 import Header from "./Header";
@@ -12,8 +15,42 @@ import SidebarMenu from "./SidebarMenu";
 export default function LayoutFrame ({children,}: {children: React.ReactNode}){
 
   const {isLoading,user,error} = useUser()
+  
+  const fetcher = async (url:string) => {
+    if (user) {
+      
+      const myHeaders = new Headers();
+      myHeaders.append("Content-Type", "application/json");
+      
+      const raw = JSON.stringify({
+        "userName": user?.nickname,
+        "avatar_url": user?.picture
+      });
+      
+      const requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+      };
+      
+      const res = await fetch(url, requestOptions)
 
-    return(
+      if (res.status === 201) {
+        
+        const data = await res.json()
+        console.log(data);
+        
+      }
+      
+
+
+    }
+    
+  }
+  const { data } = useSWR(()=> user && 'http://localhost:8080/users/', fetcher)
+
+  
+  return(
     <>
 
     <div className='m-auto flex justify-between items-center w-[100%] h-[5%] bg-indigo-500 p-2'>
@@ -47,6 +84,7 @@ export default function LayoutFrame ({children,}: {children: React.ReactNode}){
                 <button className="z-90 absolute bottom-10 right-8 bg-indigo-500 w-20 h-20 rounded-full drop-shadow-lg flex justify-center items-center text-white text-4xl hover:bg-indigo-700 hover:drop-shadow-2xl hover:animate-bounce duration-300"> + </button>
               </Link>
               }
+              <button className="z-90 absolute bottom-10 right-8 bg-indigo-500 w-20 h-20 rounded-full drop-shadow-lg flex justify-center items-center text-white text-4xl hover:bg-indigo-700 hover:drop-shadow-2xl hover:animate-bounce duration-300"> + </button>
           </div>
 </>
     )
